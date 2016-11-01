d1492d2c5c0aeec62b77e30e3a38fd01b25bf69b
meraklis56 <meraklis56@gmail.com>
2016-11-01 19:19:48 +0100

    added shark icon

--- EmbeddedSystems/src/embeddedsystems/EmbeddedSystems.java
+++ EmbeddedSystems/src/embeddedsystems/EmbeddedSystems.java
@@ -5,11 +5,20 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -52,6 +61,8 @@ class MyPanel extends JPanel {
     int gap_circle_radius = 2900;
     int swi_circle_radius = 1100;
 
+    Image img;
+
     int sensor_range = 100;
     ArrayList<Double[]> sensors;
 
@@ -76,58 +87,70 @@ class MyPanel extends JPanel {
         g2d.setRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
         //anti-aliasing
 
-        g2d.setColor(Color.YELLOW);
-        g2d.drawRect(0, 0, beach_width / pixel_ratio, model_height / pixel_ratio);
-        g2d.fillRect(0, 0, beach_width / pixel_ratio, model_height / pixel_ratio);
-        //Beach Rectangle
+        for (int k = 0; k < 10; k++) {
+            super.paintComponent(g);
+            g2d.setColor(Color.YELLOW);
+            g2d.drawRect(0, 0, beach_width / pixel_ratio, model_height / pixel_ratio);
+            g2d.fillRect(0, 0, beach_width / pixel_ratio, model_height / pixel_ratio);
+            //Beach Rectangle
 
-        g2d.setColor(new Color(0, 230, 118));
-        g2d.fillArc((-((gap_circle_radius / pixel_ratio) / 2) + (beach_width / pixel_ratio)), ((model_height / pixel_ratio) - (gap_circle_radius / pixel_ratio)) / 2, (gap_circle_radius / pixel_ratio), (gap_circle_radius / pixel_ratio), -90, 180);
-        //Gap Area
+            g2d.setColor(new Color(0, 230, 118));
+            g2d.fillArc((-((gap_circle_radius / pixel_ratio) / 2) + (beach_width / pixel_ratio)), ((model_height / pixel_ratio) - (gap_circle_radius / pixel_ratio)) / 2, (gap_circle_radius / pixel_ratio), (gap_circle_radius / pixel_ratio), -90, 180);
+            //Gap Area
 
-        g2d.setColor(new Color(3, 155, 229));
-        g2d.fillArc((-((swi_circle_radius / pixel_ratio) / 2) + (beach_width / pixel_ratio)), ((model_height / pixel_ratio) - (swi_circle_radius / pixel_ratio)) / 2, (swi_circle_radius / pixel_ratio), (swi_circle_radius / pixel_ratio), -90, 180);
-        //Swimming Area
+            g2d.setColor(new Color(3, 155, 229));
+            g2d.fillArc((-((swi_circle_radius / pixel_ratio) / 2) + (beach_width / pixel_ratio)), ((model_height / pixel_ratio) - (swi_circle_radius / pixel_ratio)) / 2, (swi_circle_radius / pixel_ratio), (swi_circle_radius / pixel_ratio), -90, 180);
+            //Swimming Area
 
-        g2d.setColor(Color.BLACK);
-        g2d.drawString("Beach", 30, (model_height / pixel_ratio) / 2);
-        //Deployment Area Text
-        g2d.drawString("Swimming Area", 190, (model_height / pixel_ratio) / 2);
-        //Swimming Area Text
-        g2d.drawString("Gap", 370, (model_height / pixel_ratio) / 2);
-        //Gap Text
-        g2d.drawString("Deployment Area", 500, (model_height / pixel_ratio) / 2);
-        //Deployment Area Text
+            g2d.setColor(Color.BLACK);
+            g2d.drawString("Beach", 30, (model_height / pixel_ratio) / 2);
+            //Deployment Area Text
+            g2d.drawString("Swimming Area", 190, (model_height / pixel_ratio) / 2);
+            //Swimming Area Text
+            g2d.drawString("Gap", 370, (model_height / pixel_ratio) / 2);
+            //Gap Text
+            g2d.drawString("Deployment Area", 500, (model_height / pixel_ratio) / 2);
+            //Deployment Area Text
 
-        g2d.drawString("Swimming Area Radius: " + swi_circle_radius + "m", win_width - 620, 20);
-        g2d.drawString("Gap Darius: " + gap_circle_radius + "m", win_width - 620, 40);
-        g2d.drawString("Number of Sensors: " + numberSensors, win_width - 620, 60);
-        g2d.drawString("Sensor Range: " + sensor_range + "m", win_width - 620, 80);
+            g2d.drawString("Swimming Area Radius: " + swi_circle_radius + "m", win_width - 620, 20);
+            g2d.drawString("Gap Darius: " + gap_circle_radius + "m", win_width - 620, 40);
+            g2d.drawString("Number of Sensors: " + numberSensors, win_width - 620, 60);
+            g2d.drawString("Sensor Range: " + sensor_range + "m", win_width - 620, 80);
 
-        Point center = new Point(beach_width / pixel_ratio, (model_height / pixel_ratio) / 2 - 5); //-5 is a fix
+            Point center = new Point(beach_width / pixel_ratio, (model_height / pixel_ratio) / 2 - 5); //-5 is a fix
 
-        int radius = ((gap_circle_radius / pixel_ratio) / 2);
-        for (int i = 0; i < (numberSensors * 2) + 1; i++) {
-            double fi = 2 * Math.PI * i / (numberSensors * 2) + 1;
-            double x = radius * Math.sin(fi + Math.PI) + center.getX();
-            double y = radius * Math.cos(fi + Math.PI) + center.getY();
-            if ((x >= beach_width / pixel_ratio - 4)) { //-4 is a fix. Moving to y axis
-                g2d.fill(new Ellipse2D.Double(x, y, 8, 8));
-                sensors.add(new Double[]{x, y});
+            int radius = ((gap_circle_radius / pixel_ratio) / 2);
+            for (int i = 0; i < (numberSensors * 2) + 1; i++) {
+                double fi = 2 * Math.PI * i / (numberSensors * 2) + 1;
+                double x = radius * Math.sin(fi + Math.PI) + center.getX();
+                double y = radius * Math.cos(fi + Math.PI) + center.getY();
+                if ((x >= beach_width / pixel_ratio - 4)) { //-4 is a fix. Moving to y axis
+                    g2d.fill(new Ellipse2D.Double(x, y, 8, 8));
+                    sensors.add(new Double[]{x, y});
+                }
             }
-        }
-        //placing sensors on deployment circle
-        for (Double[] sensor : sensors) {
-            g2d.setColor(new Color(224, 64, 251, 200));
-            g2d.fill(new Ellipse2D.Double(sensor[0]-6, sensor[1]-6, sensor_range/pixel_ratio, sensor_range/pixel_ratio));
-        }
-        //drawing sensors' range
+            //placing sensors on deployment circle
+            for (Double[] sensor : sensors) {
+                g2d.setColor(new Color(224, 64, 251, 200));
+                g2d.fill(new Ellipse2D.Double(sensor[0] - 6, sensor[1] - 6, sensor_range / pixel_ratio, sensor_range / pixel_ratio));
+            }
+            //drawing sensors' range
 
-        g2d.setColor(Color.BLACK);
-        g2d.fill(new Ellipse2D.Double((beach_width / pixel_ratio) + (swi_circle_radius / pixel_ratio) / 2 - 5, ((model_height / pixel_ratio) / 2), 8, 8));
-        //Swimmers point 1
+            g2d.setColor(Color.BLACK);
+            g2d.fill(new Ellipse2D.Double((beach_width / pixel_ratio) + (swi_circle_radius / pixel_ratio) / 2 - 5, ((model_height / pixel_ratio) / 2), 8, 8));
+            //Swimmers point 1
 
-        g2d.fill(new Ellipse2D.Double((beach_width / pixel_ratio), ((model_height / pixel_ratio) / 2) - (swi_circle_radius / pixel_ratio) / 2 - 5, 8, 8));
+            g2d.fill(new Ellipse2D.Double((beach_width / pixel_ratio), ((model_height / pixel_ratio) / 2) - (swi_circle_radius / pixel_ratio) / 2 - 5, 8, 8));
         //Swimmers point 2
+
+            try {
+                img = ImageIO.read(new File("shark.png"));
+                g2d.drawImage(img, 500 + (k*10), 500 + (k*10), null);
+            } catch (IOException ex) {
+                Logger.getLogger(MyPanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+
     }
 }
